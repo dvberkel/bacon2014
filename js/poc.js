47a922b7fb0b5f56f06813ed5beec96780a390c9
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-02-12 16:50:10 +0100

    create a skeletal proof of concept

---
+++ js/poc.js
@@ -0,0 +1,10 @@
+var RSA = (function(Reveal, BigNumber){
+    var RSA = {};
+
+    Reveal.addEventListener('poc', function(){
+	var span = document.getElementById('number');
+	span.textContent = '37';
+    });
+
+    return RSA;
+})(Reveal, BigNumber);
