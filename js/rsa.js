9e9dc40dc2f0e5239a3d3f1029848837476160ba
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-02-12 20:39:51 +0100

    create an Observable prototype

--- js/rsa.js
+++ js/rsa.js
@@ -1,3 +1,18 @@
 var RSA = (function(){
-    return {};
+    var RSA = {};
+
+    var Observable = RSA.Observable = function(){
+	this.observers = [];
+    };
+    Observable.prototype.addObserver = function(observer){
+	this.observers.push(observer);
+    };
+    Observable.prototype.notify = function(){
+	this.observers.forEach(function(observer){
+	    observer.call(this, this);
+	}.bind(this));
+    };
+
+
+    return RSA;
 })();
--- spec/rsa_spec.js
+++ spec/rsa_spec.js
@@ -2,4 +2,31 @@ describe('RSA', function(){
     it('should be defined', function(){
 	expect(RSA).toBeDefined();
     });
+
+    describe('Observerable', function(){
+	it('should be defined', function(){
+	    expect(RSA.Observable).toBeDefined();
+	});
+
+	it('should create observalbes', function(){
+	    expect(new RSA.Observable()).toBeDefined();
+	});
+
+	it('should register observers', function(){
+	    var observable = new RSA.Observable();
+
+	    observable.addObserver(function(){});
+	});
+
+	it('should notify observers', function(){
+	    var isCalled = false;
+	    var observable = new RSA.Observable();
+	    observable.addObserver(function(){ isCalled = true; });
+
+	    observable.notify();
+
+	    expect(isCalled).toBeTruthy();
+	});
+
+    });
 });
