836e2332c80e08e82607818e8b69ad23313a28f6
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-02-12 17:11:32 +0100

    add model and view

--- js/poc.js
+++ js/poc.js
@@ -1,9 +1,28 @@
 var RSA = (function(Reveal, BigNumber){
     var RSA = {};
 
+    RSA.BigNumber = function(number){
+	this.source = BigNumber(number);
+    };
+
+    var View = RSA.BigNumberView = function(parent, model){
+	this.parent = parent;
+	this.model = model;
+	this.update();
+    };
+    View.prototype.update = function(){
+	var container = this.container();
+	container.textContent = this.model.source.toString();
+    }
+    View.prototype.container = function(){
+	return this.parent;
+    }
+
+    var n = new RSA.BigNumber('51');
+
     Reveal.addEventListener('poc', function(){
 	var span = document.getElementById('number');
-	span.textContent = '37';
+	new RSA.BigNumberView(span, n);
     });
 
     return RSA;
