c0a636a37303ea5db37d69f87a10dacf437aad49
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-02-12 19:12:29 +0100

    make a product model

--- js/poc.js
+++ js/poc.js
@@ -19,11 +19,27 @@ var RSA = (function(Reveal, BigNumber){
     };
     Model.prototype = new Observable();
     Model.prototype.set = function(number) {
-	this.source = BigNumber(number);
+	this.setSource(BigNumber(number));
+    };
+    Model.prototype.setSource = function(bigNumber){
+	this.source = bigNumber;
 	this.notify();
     }
 
-    var View = RSA.BigNumberView = function(parent, model){
+    var Product = RSA.Product = function(a, b){
+	RSA.BigNumber.call(this);
+	this.a = a;
+	this.b = b;
+	this.a.addObserver(this.calculate.bind(this));
+	this.b.addObserver(this.calculate.bind(this));
+	this.calculate();
+    };
+    Product.prototype = new Model();
+    Product.prototype.calculate = function(){
+	this.setSource(this.a.source.times(this.b.source));
+    }
+
+    View = RSA.BigNumberView = function(parent, model){
 	this.parent = parent;
 	this.model = model;
 	this.model.addObserver(this.update.bind(this));
@@ -37,11 +53,21 @@ var RSA = (function(Reveal, BigNumber){
 	return this.parent;
     }
 
+    var m = new RSA.BigNumber('37');
     var n = new RSA.BigNumber('51');
+    var product = new RSA.Product(m, n);
 
     Reveal.addEventListener('poc', function(){
 	var span = document.getElementById('number');
+	var mSpan = document.getElementById('m');
+	var nSpan = document.getElementById('n');
+	var productSpan = document.getElementById('product');
+
 	new RSA.BigNumberView(span, n);
+	new RSA.BigNumberView(mSpan, m);
+	new RSA.BigNumberView(nSpan, n);
+	new RSA.BigNumberView(productSpan, product);
+
 	setTimeout(function(){
 	    n.set(37);
 	}, 2000);
