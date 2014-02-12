e1e501951c10dffa1399ac2fb2bd2b1450585afa
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-02-12 17:23:23 +0100

    observer changes of model

--- js/poc.js
+++ js/poc.js
@@ -1,13 +1,32 @@
 var RSA = (function(Reveal, BigNumber){
     var RSA = {};
 
-    RSA.BigNumber = function(number){
-	this.source = BigNumber(number);
+    var Observable = RSA.Observable = function(){
+	this.observers = [];
     };
+    Observable.prototype.addObserver = function(callback){
+	this.observers.push(callback);
+    };
+    Observable.prototype.notify = function(){
+	this.observers.forEach(function(callback){
+	    callback.call(this, this);
+	});
+    }
+
+    var Model = RSA.BigNumber = function(number){
+	Observable.call(this);
+	this.set(number || '1');
+    };
+    Model.prototype = new Observable();
+    Model.prototype.set = function(number) {
+	this.source = BigNumber(number);
+	this.notify();
+    }
 
     var View = RSA.BigNumberView = function(parent, model){
 	this.parent = parent;
 	this.model = model;
+	this.model.addObserver(this.update.bind(this));
 	this.update();
     };
     View.prototype.update = function(){
@@ -23,6 +42,9 @@ var RSA = (function(Reveal, BigNumber){
     Reveal.addEventListener('poc', function(){
 	var span = document.getElementById('number');
 	new RSA.BigNumberView(span, n);
+	setTimeout(function(){
+	    n.set(37);
+	}, 2000);
     });
 
     return RSA;
