1fd6e0c1b21cf36a04fa7762355f2781a5c002ac
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-03-06 17:24:18 +0100

    demonstrate other capabilities of RSA module

--- js/poc.js
+++ js/poc.js
@@ -2,18 +2,31 @@
 (function (Reveal, RSA) {
     'use strict';
 
+    var power = new RSA.Number('5')
+
     var p = new RSA.Number('37');
-    var q = new RSA.Number('51');
+    var q = new RSA.Number('53');
     var product = new RSA.Product(p, q);
+    var difference = new RSA.Difference(q, p)
+    var modulo = new RSA.Modulus(q, p)
+    var powerMod = new RSA.PowerMod(q, power, p);
 
     Reveal.addEventListener('poc', function () {
         var pSpan = document.getElementById('p');
         var qSpan = document.getElementById('q');
+        var powerSpan = document.getElementById('power');
         var productSpan = document.getElementById('product');
+        var differenceSpan = document.getElementById('difference');
+	var moduloSpan = document.getElementById('modulo');
+	var powerModSpan = document.getElementById('powermod');
 
         new RSA.NumberView(pSpan, p);
         new RSA.NumberView(qSpan, q);
+        new RSA.NumberView(powerSpan, power);
         new RSA.NumberView(productSpan, product);
+	new RSA.NumberView(differenceSpan, difference);
+	new RSA.NumberView(moduloSpan, modulo);
+	new RSA.NumberView(powerModSpan, powerMod);
 
         setTimeout(function () {
             p.set(3);
