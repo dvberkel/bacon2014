75d169e134bc3ca3bd61cfee27ac50e1d6899d3e
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-03-06 17:15:02 +0100

    let poc use RSA

--- js/poc.js
+++ js/poc.js
@@ -1,77 +1,22 @@
-var RSA = (function(Reveal, BigNumber){
-    var RSA = {};
+/*global Reveal, RSA, document, setTimeout*/
+(function (Reveal, RSA) {
+    'use strict';
 
-    var Observable = RSA.Observable = function(){
-	this.observers = [];
-    };
-    Observable.prototype.addObserver = function(callback){
-	this.observers.push(callback);
-    };
-    Observable.prototype.notify = function(){
-	this.observers.forEach(function(callback){
-	    callback.call(this, this);
-	});
-    }
+    var p = new RSA.Number('37');
+    var q = new RSA.Number('51');
+    var product = new RSA.Product(p, q);
 
-    var Model = RSA.BigNumber = function(number){
-	Observable.call(this);
-	this.set(number || '1');
-    };
-    Model.prototype = new Observable();
-    Model.prototype.set = function(number) {
-	this.setSource(BigNumber(number));
-    };
-    Model.prototype.setSource = function(bigNumber){
-	this.source = bigNumber;
-	this.notify();
-    }
+    Reveal.addEventListener('poc', function () {
+        var pSpan = document.getElementById('p');
+        var qSpan = document.getElementById('q');
+        var productSpan = document.getElementById('product');
 
-    var Product = RSA.Product = function(a, b){
-	RSA.BigNumber.call(this);
-	this.a = a;
-	this.b = b;
-	this.a.addObserver(this.calculate.bind(this));
-	this.b.addObserver(this.calculate.bind(this));
-	this.calculate();
-    };
-    Product.prototype = new Model();
-    Product.prototype.calculate = function(){
-	this.setSource(this.a.source.times(this.b.source));
-    }
+        new RSA.NumberView(pSpan, p);
+        new RSA.NumberView(qSpan, q);
+        new RSA.NumberView(productSpan, product);
 
-    View = RSA.BigNumberView = function(parent, model){
-	this.parent = parent;
-	this.model = model;
-	this.model.addObserver(this.update.bind(this));
-	this.update();
-    };
-    View.prototype.update = function(){
-	var container = this.container();
-	container.textContent = this.model.source.toString();
-    }
-    View.prototype.container = function(){
-	return this.parent;
-    }
-
-    var m = new RSA.BigNumber('37');
-    var n = new RSA.BigNumber('51');
-    var product = new RSA.Product(m, n);
-
-    Reveal.addEventListener('poc', function(){
-	var span = document.getElementById('number');
-	var mSpan = document.getElementById('m');
-	var nSpan = document.getElementById('n');
-	var productSpan = document.getElementById('product');
-
-	new RSA.BigNumberView(span, n);
-	new RSA.BigNumberView(mSpan, m);
-	new RSA.BigNumberView(nSpan, n);
-	new RSA.BigNumberView(productSpan, product);
-
-	setTimeout(function(){
-	    n.set(37);
-	}, 2000);
+        setTimeout(function () {
+            p.set(3);
+        }, 2000);
     });
-
-    return RSA;
-})(Reveal, BigNumber);
+})(Reveal, RSA);
