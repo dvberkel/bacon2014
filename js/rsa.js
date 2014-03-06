f849aa689016265f84bb3d02506997cb82c67865
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-03-06 17:10:05 +0100

    make rsa.js pass jshint

--- js/rsa.js
+++ js/rsa.js
@@ -1,82 +1,85 @@
-var RSA = (function(BigNumber){
+/*global BigNumber, window:true*/
+window.RSA = (function (BigNumber) {
+    'use strict';
+
     var RSA = {};
 
-    var Observable = RSA.Observable = function(){
-	this.observers = [];
+    var Observable = RSA.Observable = function () {
+        this.observers = [];
     };
-    Observable.prototype.addObserver = function(observer){
-	this.observers.push(observer);
+    Observable.prototype.addObserver = function (observer) {
+        this.observers.push(observer);
     };
-    Observable.prototype.notify = function(){
-	this.observers.forEach(function(observer){
-	    observer.call(this, this);
-	}.bind(this));
+    Observable.prototype.notify = function () {
+        this.observers.forEach(function (observer) {
+            observer.call(this, this);
+        }.bind(this));
     };
 
-    var rsaNumber = RSA.Number = function(number){
-	Observable.call(this);
-	this.set(number || '1');
+    var rsaNumber = RSA.Number = function (number) {
+        Observable.call(this);
+        this.set(number || '1');
     };
     rsaNumber.prototype = new Observable();
-    rsaNumber.prototype.set = function(number){
-	this.setSource(new BigNumber(number));
+    rsaNumber.prototype.set = function (number) {
+        this.setSource(new BigNumber(number));
     };
-    rsaNumber.prototype.setSource = function(bigNumber){
-	this.source = bigNumber;
-	this.notify();
+    rsaNumber.prototype.setSource = function (bigNumber) {
+        this.source = bigNumber;
+        this.notify();
     };
 
-    var derivedNumber = function(calculation){
-	var DerivedNumber = function(){
-	    var callback = this.calculate.bind(this);
-	    this.input = [];
-	    for(var index = 0; index < arguments.length; index++){
-		var input = arguments[index];
-		input.addObserver(callback);
-		this.input.push(input);
-	    }
-	    this.calculate();
-	};
-	DerivedNumber.prototype = new RSA.Number();
-	DerivedNumber.prototype.calculate = calculation;
-	return DerivedNumber;
-    }
+    var derivedNumber = function (calculation) {
+        var DerivedNumber = function () {
+            var callback = this.calculate.bind(this);
+            this.input = [];
+            for (var index = 0; index < arguments.length; index++) {
+                var input = arguments[index];
+                input.addObserver(callback);
+                this.input.push(input);
+            }
+            this.calculate();
+        };
+        DerivedNumber.prototype = new RSA.Number();
+        DerivedNumber.prototype.calculate = calculation;
+        return DerivedNumber;
+    };
 
-    var Product = RSA.Product = derivedNumber(function(){
-	this.setSource(this.input[0].source.times(this.input[1].source));
+    RSA.Product = derivedNumber(function () {
+        this.setSource(this.input[0].source.times(this.input[1].source));
     });
-    var Sum = RSA.Sum = derivedNumber(function(){
-	this.setSource(this.input[0].source.plus(this.input[1].source));
+    RSA.Sum = derivedNumber(function () {
+        this.setSource(this.input[0].source.plus(this.input[1].source));
     });
-    var Difference = RSA.Difference = derivedNumber(function(){
-	this.setSource(this.input[0].source.minus(this.input[1].source));
+    RSA.Difference = derivedNumber(function () {
+        this.setSource(this.input[0].source.minus(this.input[1].source));
     });
-    var Modulus = RSA.Modulus = derivedNumber(function(){
-	this.setSource(this.input[0].source.modulo(this.input[1].source));
+    RSA.Modulus = derivedNumber(function () {
+        this.setSource(this.input[0].source.modulo(this.input[1].source));
     });
-    var PowerMod = RSA.PowerMod = (function(){
-	var zero = BigNumber('0');
-	var two = BigNumber('2');
-	var quotient2 = function(source) { return source.dividedBy(two).floor(); }
-	return derivedNumber(function(){
-	    var result = BigNumber('1');
-	    var square = this.input[0].source;
-	    var q = quotient2(this.input[1].source);
-	    var r = this.input[1].source.modulo(two);
-	    var modulus = this.input[2].source;
-	    while (! q.equals(zero)) {
-		if (! r.equals(zero)) {
-		    result = result.times(square).modulo(modulus);
-		}
-		square = square.times(square);
-		r = q.modulo(two);
-		q = quotient2(q);
-	    }
-	    if (! r.equals(zero)) {
-		result = result.times(square).modulo(modulus);
-	    }
-	    this.setSource(result);
-    })
+    RSA.PowerMod = (function () {
+        var zero = new BigNumber('0');
+        var two = new BigNumber('2');
+        var quotient2 = function (source) { return source.dividedBy(two).floor(); };
+        return derivedNumber(function () {
+            var result = new BigNumber('1');
+            var square = this.input[0].source;
+            var q = quotient2(this.input[1].source);
+            var r = this.input[1].source.modulo(two);
+            var modulus = this.input[2].source;
+            while (! q.equals(zero)) {
+                if (! r.equals(zero)) {
+                    result = result.times(square).modulo(modulus);
+                }
+                square = square.times(square);
+                r = q.modulo(two);
+                q = quotient2(q);
+            }
+            if (! r.equals(zero)) {
+                result = result.times(square).modulo(modulus);
+            }
+            this.setSource(result);
+        });
     })();
 
     return RSA;
