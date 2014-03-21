df24ba9a611f1569fd89ecf1c97b551ee8c71a37
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-03-21 11:52:42 +0100

    introduce a sieve number

--- js/sieve.js
+++ js/sieve.js
@@ -1,7 +1,29 @@
-/*global window:true*/
-window.Sieve = (function () {
+/*global Observable, window:true*/
+window.Sieve = (function (Observable) {
     'use strict';
     var Sieve = {};
 
+    var Number = Sieve.Number = function (n) {
+        Observable.call(this);
+        this.n = n;
+        this.ds = [];
+    };
+    Number.prototype = new Observable();
+    Number.prototype.value = function () {
+        return this.n;
+    };
+    Number.prototype.divisors = function () {
+        return this.ds.slice(0);
+    };
+    Number.prototype.addDivisor = function (d) {
+        this.ds.push(d);
+        this.notify();
+    };
+
+    var Model = Sieve.Model = function () {
+        Observable.call(this);
+    };
+    Model.prototype = new Observable();
+
     return Sieve;
-})();
+})(Observable);
--- spec/sieve_spec.js
+++ spec/sieve_spec.js
@@ -2,4 +2,64 @@ describe('Sieve', function(){
     it('should be defined', function(){
 	expect(Sieve).toBeDefined();
     });
+
+    describe('Number', function(){
+	var testValue = 12;
+	var number;
+
+	beforeEach(function(){
+	    number = new Sieve.Number(testValue);
+	});
+
+	it('should be defined', function(){
+	    expect(Sieve.Number).toBeDefined();
+	});
+
+	it('should be observable', function(){
+	    var n = new Sieve.Number(2);
+
+	    expect(n instanceof Observable).toBeTruthy();
+	});
+
+	it('should have a value', function(){
+	    expect(number.value()).toBe(testValue)
+	});
+
+	it('should start with an empty list of divisors', function(){
+	    expect(number.divisors().length).toBe(0);
+	});
+
+	it('should add divisors', function(){
+	    number.addDivisor(2);
+
+	    expect(number.divisors()).toContain(2);
+
+	    number.addDivisor(3);
+
+	    expect(number.divisors()).toContain(2);
+	    expect(number.divisors()).toContain(3);
+	});
+
+	it('should notify when a divisor is added', function(){
+	    var isCalled = false;
+	    number.addObserver(function(){ isCalled = true; });
+
+	    number.addDivisor(2);
+
+	    expect(isCalled).toBeTruthy();
+	});
+    });
+
+
+    describe('Model', function(){
+	it('should be defined', function(){
+	    expect(Sieve.Model).toBeDefined();
+	});
+
+	it('should be observable', function(){
+	    var model = new Sieve.Model(10);
+
+	    expect(model instanceof Observable).toBeTruthy();
+	});
+    });
 });
