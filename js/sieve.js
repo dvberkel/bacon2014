d2ad0d32255bf5243256cc147f4fad64e54b2628
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-03-21 16:22:51 +0100

    create a sieve of Erotosthenes

--- js/sieve.js
+++ js/sieve.js
@@ -3,27 +3,60 @@ window.Sieve = (function (Observable) {
     'use strict';
     var Sieve = {};
 
-    var Number = Sieve.Number = function (n) {
+    var SieveNumber = Sieve.Number = function (n) {
         Observable.call(this);
         this.n = n;
         this.ds = [];
     };
-    Number.prototype = new Observable();
-    Number.prototype.value = function () {
+    SieveNumber.prototype = new Observable();
+    SieveNumber.prototype.value = function () {
         return this.n;
     };
-    Number.prototype.divisors = function () {
+    SieveNumber.prototype.divisors = function () {
         return this.ds.slice(0);
     };
-    Number.prototype.addDivisor = function (d) {
+    SieveNumber.prototype.addDivisor = function (d) {
         this.ds.push(d);
         this.notify();
     };
 
-    var Model = Sieve.Model = function () {
+    var numbers = function (n) {
+        var result = [];
+        for (var index = 1; index <= n; index++) {
+            result.push(new SieveNumber(index));
+            if (index === 1) {
+                result[0].addDivisor(0);
+            }
+        }
+        return result;
+    };
+
+    var Model = Sieve.Model = function (n) {
         Observable.call(this);
+        this.currentIndex = 1;
+        this.n = n;
+        this.ns = numbers(n);
     };
     Model.prototype = new Observable();
-
+    Model.prototype.numbers = function () {
+        return this.ns.slice(0);
+    };
+    Model.prototype.finished = function () {
+        return 2 * this.ns[this.currentIndex].value() > this.n;
+    };
+    Model.prototype.sieveStep = function () {
+        var d = this.ns[this.currentIndex].value();
+        var index = 2;
+        while ((d * index - 1) < this.ns.length) {
+            this.ns[d * index - 1].addDivisor(d);
+            index++;
+        }
+        while (this.ns[++this.currentIndex].divisors().length > 0) {/* do nothing */}
+    };
+    Model.prototype.sieve = function () {
+        while (!this.finished()) {
+            this.sieveStep();
+        }
+    };
     return Sieve;
 })(Observable);
--- spec/sieve_spec.js
+++ spec/sieve_spec.js
@@ -52,14 +52,94 @@ describe('Sieve', function(){
 
 
     describe('Model', function(){
+	var n = 20;
+	var model;
+
+	beforeEach(function(){
+	    model = new Sieve.Model(n);
+	});
+
 	it('should be defined', function(){
 	    expect(Sieve.Model).toBeDefined();
 	});
 
-	it('should be observable', function(){
-	    var model = new Sieve.Model(10);
+ 	it('should be observable', function(){
+	    var m = new Sieve.Model(10);
 
-	    expect(model instanceof Observable).toBeTruthy();
+	    expect(m instanceof Observable).toBeTruthy();
+	});
+
+	it('should contain numbers', function(){
+	    expect(model.numbers().length).toBe(n);
+	});
+
+	it('number on should contain the special divisor \'0\'', function(){
+	    expect(model.numbers()[0].divisors()).toContain(0);
+	});
+
+	it('should start out not finished', function(){
+	    expect(model.finished()).toBeFalsy();
+	});
+
+	it('sieveStep should mark multiples of first unrecognized prime', function(){
+	    model.sieveStep();
+
+	    var numbers = model.numbers();
+	    [4, 6, 8, 10, 12, 14, 16, 18, 20].forEach(function(index){
+		expect(numbers[index - 1].divisors()).toContain(2);
+	    });
+	});
+
+	it('consequtive sieveStep should mark multiples of consequtive primes', function(){
+	    model.sieveStep();
+	    model.sieveStep();
+	    model.sieveStep();
+
+	    var numbers = model.numbers();
+	    [4, 6, 8, 10, 12, 14, 16, 18, 20].forEach(function(index){
+		expect(numbers[index - 1].divisors()).toContain(2);
+	    });
+	    [6, 9, 12, 15, 18].forEach(function(index){
+		expect(numbers[index - 1].divisors()).toContain(3);
+	    });
+	    [10, 15, 20].forEach(function(index){
+		expect(numbers[index - 1].divisors()).toContain(5);
+	    });
+	});
+
+	it('should be finished if no candidates can have divisors', function(){
+	    expect(model.finished()).toBeFalsy();
+	    model.sieveStep();
+
+	    expect(model.finished()).toBeFalsy();
+	    model.sieveStep();
+
+	    expect(model.finished()).toBeFalsy();
+	    model.sieveStep();
+
+	    expect(model.finished()).toBeFalsy();
+	    model.sieveStep();
+
+	    expect(model.finished()).toBeTruthy();
+	});
+
+	it('sieve should sieveStep until finished', function(){
+	    model.sieve();
+	    var numbers = model.numbers();
+	    [
+		{ divisor: 2, multiples: [4, 6, 8, 10, 12, 14, 16, 18, 20] },
+		{ divisor: 3, multiples: [6, 9, 12, 15] },
+		{ divisor: 5, multiples: [10, 15] },
+		{ divisor: 7, multiples: [14] },
+	    ].forEach(function(entry) {
+		entry.multiples.forEach(function(index) {
+		    expect(numbers[index - 1].divisors()).toContain(entry.divisor);
+		});
+	    });
+
+	    [2, 3, 5, 7, 11, 13, 17, 19].forEach(function(prime) {
+		expect(numbers[prime - 1].divisors().length).toBe(0);
+	    });
 	});
     });
 });
