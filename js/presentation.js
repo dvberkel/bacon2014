c2c68ee7a8fe433203daae64aa039533d9f7d2f2
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-03-26 07:29:03 +0100

    N = p * q

--- js/presentation.js
+++ js/presentation.js
@@ -4,6 +4,8 @@
 
     var p = new RSA.Number('3');
     var q = new RSA.Number('5');
+    var N = new RSA.Product(p, q);
+
     var model = new Sieve.Model(20);
 
     Reveal.addEventListener('pq', function () {
@@ -18,5 +20,11 @@
         var container = document.getElementById('prime-sieve');
 
         new Sieve.ControlView(container, model);
+    });
+
+    Reveal.addEventListener('n', function () {
+        var nView = document.getElementById('n_view');
+
+        new RSA.EditableNumberView(nView, N);
     });
 })(Reveal, RSA, Sieve);
