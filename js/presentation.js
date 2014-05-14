f5c29e83398ae740bd9ac392562acc007c8a1d46
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-05-14 16:20:23 +0200

    create a addition page

--- js/presentation.js
+++ js/presentation.js
@@ -43,6 +43,23 @@
         new RSA.NumberView(factorView, factor);
     });
 
+    Reveal.addEventListener('addition', function () {
+        var left = new RSA.Number('3');
+        var right = new RSA.Number('5');
+        var result = new RSA.Modulus(new RSA.Sum(left, right), N);
+
+	var prefix = 'addition';
+        var leftView = document.getElementById(prefix + '_left');
+        var rightView = document.getElementById(prefix + '_right');
+        var resultView = document.getElementById(prefix + '_result');
+        var modulusView = document.getElementById(prefix + '_modulus');
+
+        new RSA.EditableNumberView(leftView, left);
+        new RSA.EditableNumberView(rightView, right);
+        new RSA.NumberView(resultView, result);
+        new RSA.NumberView(modulusView, N);
+    });
+
     Reveal.addEventListener('phi', function () {
         var phiView = document.getElementById('phi_view');
 
