69f47f6b8738938a7eca7afe5bcf307d769a7d35
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-05-14 16:51:06 +0200

    create a multiplication page

--- js/presentation.js
+++ js/presentation.js
@@ -60,6 +60,23 @@
         new RSA.NumberView(modulusView, N);
     });
 
+    Reveal.addEventListener('multiplication', function () {
+        var left = new RSA.Number('3');
+        var right = new RSA.Number('5');
+        var result = new RSA.Modulus(new RSA.Product(left, right), N);
+
+	var prefix = 'multiplication';
+        var leftView = document.getElementById(prefix + '_left');
+        var rightView = document.getElementById(prefix + '_right');
+        var resultView = document.getElementById(prefix + '_result');
+        var modulusView = document.getElementById(prefix + '_modulus');
+
+        new RSA.EditableNumberView(leftView, left);
+        new RSA.EditableNumberView(rightView, right);
+        new RSA.NumberView(resultView, result);
+        new RSA.NumberView(modulusView, N);
+    });
+
     Reveal.addEventListener('phi', function () {
         var phiView = document.getElementById('phi_view');
 
